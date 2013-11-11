8f5af173cd41480c5138380389361082a36fa104
Steve Powell <steve@rabbitmq.com>
2013-11-11 15:21:04 +0000

    Speed up tests a little more.
    [v1.1.x]

--- src/test/java/com/rabbitmq/integration/jmscts/MainComplianceIT.java
+++ src/test/java/com/rabbitmq/integration/jmscts/MainComplianceIT.java
@@ -20,7 +20,7 @@ public class MainComplianceIT extends TestCase {
 
     public void testAll() throws Exception {
         if (System.getProperty("rabbit.jms.terminationTimeout") == null) {
-            System.setProperty("rabbit.jms.terminationTimeout", "5000");
+            System.setProperty("rabbit.jms.terminationTimeout", "2000");
         }
         String basedir = System.getProperty("basedir");
         ComplianceTestSuite.main(new String[] { "-output",
@@ -31,7 +31,7 @@ public class MainComplianceIT extends TestCase {
 
     public void testSelector() throws Exception {
         if (System.getProperty("rabbit.jms.terminationTimeout") == null) {
-            System.setProperty("rabbit.jms.terminationTimeout", "5000");
+            System.setProperty("rabbit.jms.terminationTimeout", "2000");
         }
         String basedir = System.getProperty("basedir");
         ComplianceTestSuite.main(new String[] { "-output",
